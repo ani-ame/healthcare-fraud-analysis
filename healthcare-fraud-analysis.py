b9f8aa368a65d52a250fae90abc4120a12f0f011
ani-ame <287491032+ani-ame@users.noreply.github.com>
2026-05-28 22:51:17 +0100

    Modify .gitignore and add business question

---
+++ healthcare-fraud-analysis.py
@@ -0,0 +1,20 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.19.3
+#   kernelspec:
+#     display_name: Python (venv)
+#     language: python
+#     name: .venv
+# ---
+
+# %% [markdown]
+# ## Business Question
+
+# %% [markdown]
+# > What patterns most reliably identify fraudulent insurance claims?
